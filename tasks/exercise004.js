93b6b2432b3c2b5d27da4a5932c0270c5cf83042
Harsheek Thanki <harsheekthanki@gmail.com>
2020-10-29 23:51:36 +0000

    pseudo code added

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -1,7 +1,14 @@
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
 
 function pigLatin(str){
-    // You code here!
+    //split string by space or punctuation into array
+    
+    //create new output array
+
+    //loop over each word and take first element of each word and put on the end and add 'ay' to it
+    
+    //output the array with the words joined back together into a sentence
+
 }
 
 module.exports = {
