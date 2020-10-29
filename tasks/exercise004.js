6342ccc1c6fb6f585d342672bb3f1210d96e8b0f
Harsheek Thanki <harsheekthanki@gmail.com>
2020-10-29 23:57:06 +0000

    code added. tests passing

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -2,13 +2,18 @@
 
 function pigLatin(str){
     //split string by space or punctuation into array
-    
+    let strArray= str.split(/\s|\b/);
+
     //create new output array
+    let newWords = [];
 
-    //loop over each word and take first element of each word and put on the end and add 'ay' to it
-    
+    //loop over each strArray element, if the first character of each strArray element is a letter, put on the end of the word and add 'ay' to it. If it is not a letter, add the curent element to the last element in newWords array  
+    for (var i = 0; i < strArray.length; i++) {
+        strArray[i][0].match(/[a-z]/i) ? newWords.push(strArray[i].slice(1)+ strArray[i].charAt(0)+ "ay") : newWords[newWords.length-1] = newWords[newWords.length-1] + strArray[i];
+      }
+
     //output the array with the words joined back together into a sentence
-
+    return newWords.join(' ');
 }
 
 module.exports = {
