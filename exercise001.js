1439975347de20a73e56d20293da7612918ca6a4
Harsheek Thanki <harsheekthanki@gmail.com>
2020-10-29 22:38:21 +0000

    commit from vscode

---
+++ exercise001.js
@@ -0,0 +1,34 @@
+// In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. 
+// Your task will be to return the sum of the numbers that occur only once.
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15.
+// More examples in the test cases below.
+
+// Good luck!
+
+function singles(arr){
+    if (arr === undefined) throw new Error("array is required");
+    
+    //reorder the array to ascending order
+    arr.sort((a, b) => a - b);
+
+    //create an empty array called uniqueArray
+    let uniqueArray = [];
+    
+    //loop through arr. On each element check if the previous or next element is equal to current element
+    arr.forEach((number, index)=> {
+    
+        if (number == arr[index-1] || number == arr[index+1]){
+    
+        } else {
+        //if not, push current elem to newArray
+            uniqueArray.push(number);
+          }
+        });
+    
+    //if uniqueArray is empty return 0 else return sum of elements in newArray
+    return (uniqueArray.length == 0) ? 0: uniqueArray.reduce((total, currentNumber ) => total + currentNumber);
+    }
+
+module.exports = {
+    singles
+};
